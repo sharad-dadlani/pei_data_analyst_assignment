8c8fb9b8fe915aefdcdb8f96df5bca2927c6f756
sharad-dadlani <sharad.dadlani92@gmail.com>
2025-08-19 15:59:02 +0530

    Update and rename Data Validation.sql to 1. Data Validation.sql

--- 1. Data Validation.sql
+++ 1. Data Validation.sql
@@ -1,11 +1,11 @@
 --1.Verify the accuracy, completeness, and reliability of source data. Show your results in a SQL or Python output.
 
-Accuracy - 
-Checking for negative revenue (Data Accuracy)
-Queries
+-- Accuracy 
+-- Checking for negative revenue (Data Accuracy)
+-- Queries
 
 
-a)     
+-- a)   
 SELECT *
 FROM `pei-data-analyst-task.pei_data.order`
 where Amount <0
@@ -13,8 +13,7 @@ where Amount <0
 
 Output : No Data (Data Accuracy and Reliability )
 
-b) Check for age<0 or age >100  (Data Accuracy)
-
+-- b) Check for age<0 or age >100  (Data Accuracy)
 
 Select *
  	FROM
@@ -24,13 +23,7 @@ Select *
 
 Output : No Data
 
-
-
-
-
-
-c) 	checking duplicate orders
-
+-- c) 	checking duplicate orders
 
 SELECT order_id, count(*) as order_count
 FROM `pei-data-analyst-task.pei_data.order`
@@ -38,11 +31,10 @@ group by 1
 having count(*)>1
 order by 2 desc
 
-
 Output : No Data
 
-
-d) Checking customer level duplicate 
+-- d) Checking customer level duplicate 
+ 
 SELECT customer_id,count(*)  as customer_count
  FROM `pei-data-analyst-task.pei_data.customer`
 group by 1
@@ -50,7 +42,8 @@ having count(*)>1
 order by 2 desc
 
 
-e) checking Row Level Duplicate for customer_data
+-- e) checking Row Level Duplicate for customer_data
+ 
 SELECT customer_id,
 first as first_name,
 last as last_name,
@@ -66,7 +59,7 @@ order by 6 desc
 Output : No Data
 
 
-f) Checking Shipping Duplicates for Shipping Data 
+-- f) Checking Shipping Duplicates for Shipping Data 
 
 
 SELECT shipping_id,
@@ -80,15 +73,12 @@ order by 2 desc
 Output : No Data
 
 
-No Duplicates in the order, customer and shipping data source
-Data is accurate and reliable  -  Unique Records 
+-- No Duplicates in the order, customer and shipping data source
+-- Data is accurate and reliable  -  Unique Records 
 
 
-g) Primary Foreign Key Relationship check between Data Sources 
-
-
-
-
+-- g) Primary Foreign Key Relationship check between Data Sources 
+ 
 SELECT *
 FROM
 `pei-data-analyst-task.pei_data.order` o
@@ -99,7 +89,7 @@ where c.customer_id is null
 Output : No Data 
 Data is Accurate and Reliable 
 
-h) Completeness Check    
+-- h) Completeness Check    
 
  SELECT count(*) as null_count_o
 FROM `pei-data-analyst-task.pei_data.order` 
@@ -109,11 +99,12 @@ null_count
 0
 
 Select count(*) as null_count_c
- FROM `pei-data-analyst-task.pei_data.customer`
+FROM `pei-data-analyst-task.pei_data.customer`
 where customer_id is null or first is null or last is null or age is null or country is null
 Output - 
 null_count
 0
+
 
 SELECT count(*) as null_count_s
 FROM `pei-data-analyst-task.pei_data.shipping`
